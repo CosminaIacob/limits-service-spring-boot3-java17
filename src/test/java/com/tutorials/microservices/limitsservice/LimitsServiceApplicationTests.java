2b1b96214465028faad03b53343d11826baf1651
coiacob <coiacob@pentalog.com>
2023-10-06 15:05:24 +0300

    Initial commit

---
+++ src/test/java/com/tutorials/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
